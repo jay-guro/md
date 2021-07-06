4b6d1593769da43e3c79abc645749b8a30805c93
Jason Guro <62923544+jay-guro@users.noreply.github.com>
2021-07-06 13:15:55 -0400

    update

---
+++ js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+// Burger
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
+
+// ParticlesJS
+window.onload = function () {
+    particlesJS.load("particles-js", "js/particles.json", function () {
+    });
+};
+
+// Loader
+$(document).ready(function () {
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 1500);
+});
+
+// ParticlesJS
+window.onload = function () {
+  particlesJS.load('tsparticles-js_cs', 'js/particles.json', function () {
+  });
+};
